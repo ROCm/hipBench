16384f30e9c9009f7e91116ff2a9f8d2c52adc6a
NorouziArab, Mohammad <Mohammad.NorouziArab@amd.com>
2025-12-10 12:47:37 +0100

    nvbench/json_printer.cu: Fix error: cannot convert 'const nlohmann::basic_json<>' to 'value_t' without a conversion operator

--- nvbench/json_printer.cu
+++ nvbench/json_printer.cu
@@ -16,6 +16,28 @@
  *  limitations under the License.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <nvbench/axes_metadata.cuh>
 #include <nvbench/benchmark_base.cuh>
 #include <nvbench/config.cuh>
@@ -467,7 +489,9 @@ void json_printer::do_print_benchmark_list(const benchmark_vector &benches)
 
     // We have to ensure that the axes are represented as an array, not an
     // nil object when there are no axes.
-    auto &axes = bench["axes"] = nlohmann::json::array();
+    // NOTE(HIP/AMD): Use the type of bench to construct the array
+    using bench_t = std::decay_t<decltype(bench)>;
+    auto &axes = bench["axes"] = bench_t::array();
 
     for (const auto &axis_ptr : bench_ptr->get_axes().get_axes())
     {
