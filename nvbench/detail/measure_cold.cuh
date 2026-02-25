c623a92e64a7b1dbacc25e1246b04ee98c6cafc9
mnorouzi_amdeng <mnorouzi@amd.com>
2026-02-25 12:14:43 +0000

    nvbench/detail/measure_cold.cuh: Use cuda_runtime header inside nvbench

--- nvbench/detail/measure_cold.cuh
+++ nvbench/detail/measure_cold.cuh
@@ -18,7 +18,7 @@
 
 // MIT License
 //
-// Modifications Copyright (C) 2023-2025 Advanced Micro Devices, Inc. All rights reserved.
+// Modifications Copyright (C) 2023-2026 Advanced Micro Devices, Inc. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -53,8 +53,6 @@
 #include <nvbench/launch.cuh>
 #include <nvbench/stopping_criterion.cuh>
 #include <nvbench/types.cuh>
-
-#include <cuda_runtime.h>
 
 #include <nvbench/cuda_runtime.h>
 
